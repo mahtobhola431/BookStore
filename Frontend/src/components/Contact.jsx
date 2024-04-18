ee2162453fbd2d7d0eea13124dae5eec6da7aef0
mahtobhola431 <72021931+mahtobhola431@users.noreply.github.com>
2024-04-18 14:44:39 +0530

    frontend completed and backend started

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -80,7 +80,7 @@ const Contact = () => {
               <textarea
                 name="message"
                 id="message"
-                rows={4}
+                rows={2}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 defaultValue={''}
               />
